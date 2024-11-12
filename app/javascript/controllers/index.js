46ecfa067779f5b3adfde158b8904f4bcee9b421
Sherif Nada <snadalive@gmail.com>
2024-11-13 00:41:16 +0400

    clean up stimulus controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
 import FlashCardController from "./flash_card_controller"
+import CardController from "./card_controller"
 
 application.register("flash-card", FlashCardController)
+application.register("card", CardController)
